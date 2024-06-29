f6bbc7142590cdd34270c96c3c2a40044facc8e8
Thomas McMahon <glassoctopus@yahoo.com>
2024-06-29 11:42:56 -0500

    Initial commit

---
+++ tasks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    temporary_field = models.BooleanField(default=True)  # Temporary field
+
+    def __str__(self):
+        return self.title
